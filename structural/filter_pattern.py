b881500a66745993530b82552eef43b37345faec
kaushik <kkaushik24@gmail.com>
2015-12-21 23:25:54 +0530

    added some logic in filter pattern

---
+++ structural/filter_pattern.py
@@ -0,0 +1,39 @@
+from abc import ABCMeta, abcmethod
+class Person:
+
+    def __init__(self, name, gender, marital_status):
+        self.name = name
+        self.gender = gender
+        self.marital_status = marital_status
+
+    def get_name(self):
+        return self.name
+    
+    def get_gender(self):
+        return self.gender
+
+    def get_marital_status(self):
+        return self.marital_status
+
+
+class Criteria:
+
+   __metaclass__ = ABCMeta
+
+    @abcmethod
+    def meet_criteria(self, person_list):
+        pass
+
+    
+
+class CriteriaMale(Criteria):
+
+    def meet_criteria(self, person_list):
+        male_person_list = []
+        for person in person_list:
+            if person.get_gender() == "Male":
+                male_person_list.append(person)
+        
+        return male_person_list
+
+        
