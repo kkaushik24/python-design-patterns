1f3593df68f2b9367e3560c829c388d36bdbfa3a
kaushik <kkaushik24@gmail.com>
2015-12-30 22:30:56 +0530

    added shapefactory and flyweeght demo in flyweight pattern

--- structural/flyweight_pattern.py
+++ structural/flyweight_pattern.py
@@ -1,4 +1,5 @@
 from abc import ABCMeta, abstractmethod
+from random import randint
 
 
 class Shape:
@@ -25,3 +26,28 @@ class Circle(Shape):
 
     def draw(self):
         print "Circle: Draw color:{0} x:{1},y:{2},radius:{3}".format(self.color, self.x, self.y, self.radius)
+
+
+class ShapeFactory:
+
+    circle_dict = {}
+
+    def get_circle(self, color):
+        circle = ShapeFactory.circle_dict.get(color)
+        if circle is None:
+            circle = Circle(color)
+            ShapeFactory.circle_dict.update({color: circle})
+            print 'creating circle of color: ' + color
+
+        return circle
+
+# flyweight demo
+if __name__ == '__main__':
+
+    colors = ['red', 'green', 'blue', 'white', 'black']
+    for color in colors:
+        circle = ShapeFactory().get_circle(color)
+        circle.set_x(randint(1, 10))
+        circle.set_y(randint(1, 10))
+        circle.set_radius(100)
+        circle.draw()
